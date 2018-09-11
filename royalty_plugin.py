bc2ba776f5cd5fe7ccc8ee8b9a2a5ef9326ec971
Vinod Kumar Sivalingam <vsivalingam@uaudio.com>
2018-09-11 16:04:43 -0700

    custom subledger fix

--- royalty_plugin.py
+++ royalty_plugin.py
@@ -20,10 +20,10 @@ def dbconnection():
     # env_var = content.decode('ascii')
     # print(env_var)
 
-    hostname = '************'
+    hostname = '***************'
     #  has to be removed once the S3 credentials bucket is setup and test to access the credentials directly from S3
     username = 'redshift'
-    psd = '************'
+    psd = '***************'
 
     conn = pymysql.connect(
         host=hostname,
@@ -97,9 +97,9 @@ def getorders():
                     ON v.vouchers_purchase_ordernum = o.entity_id
                     JOIN uaudio.sales_flat_order_item i
                     ON i.vouchers_serial = v.vouchers_serial
-                    where v.voucher_type='purchase' 
-                    # AND vouchers_purchase_date BETWEEN '2018-06-01' AND '2018-06-30' 
-                    AND o.entity_id = 1191915
+                    where v.voucher_type = 'purchase' 
+                    AND vouchers_purchase_date BETWEEN '2018-06-01' AND '2018-06-30' 
+                    # AND o.entity_id = 1191826
                     AND o.state = 'complete' AND status = 'complete'
                     """
         cursor.execute(sql)
@@ -344,14 +344,14 @@ def buildcustomdata(order, product_catalog, conn1):
     for custrec in custom_rec:
 
         ## Test
-        print("Test !!!!!")
         with conn1.cursor() as cursor1:
 
             sql=""" INSERT INTO public.royalty (SELECT purchase_type,item_type, order_id, item_id, customer_id, 
                         order_sku, voucher_serial, custom_serial, sku, created_at, (list_price)* -1 , pro_rata
-                        FROM public.royalty)
+                        FROM public.royalty
+                        WHERE order_id = %s AND item_type = 'custom')
             """
-            cursor1.execute(sql)
+            cursor1.execute(sql,(custrec['orders_id'],))
 
         ### END TEST
 
